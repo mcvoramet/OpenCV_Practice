b57389d5a2eec09f08c53449b7cb8253ea79bc1b
mcvoramet <vc2543@gmail.com>
2021-10-23 22:47:24 +0700

    fix read_file function

--- img_extract_proj.py
+++ img_extract_proj.py
@@ -3,21 +3,22 @@ import matplotlib.pyplot as plt
 import numpy as np
 import os
 
-early_DIR = r'/Users/vorametchunvattananon/Desktop/Dev/OpenCV_Project/Potato/Train/Potato___Early_blight'
-late_DIR = r'/Users/vorametchunvattananon/Desktop/Dev/OpenCV_Project/Potato/Train/Potato___Late_blight'
-healthy_DIR = r'/Users/vorametchunvattananon/Desktop/Dev/OpenCV_Project/Potato/Train/Potato___healthy'
-
-img_file_path = []
+early_DIR = 'Potato/Train/Potato___Early_blight'   # total 300 images
+late_DIR = 'Potato/Train/Potato___Late_blight'     # total 300 images
+healthy_DIR = 'Potato/Train/Potato___healthy'      # total 300 images
 
 def read_file(path):
+    img_file_path = []
     for img in os.listdir(path):
         img_file_path.append(path + '/' + img)
     
     return img_file_path
 
-img = read_file(early_DIR)
+early_img = read_file(early_DIR)
+late_img = read_file(late_DIR)
+healthy_img = read_file(healthy_DIR)
 
-img = cv.imread(img[0])
+img = cv.imread(healthy_img[0])
 cv.imshow('Potato', img)
 
 gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
