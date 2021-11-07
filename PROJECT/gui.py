766d0dd3f36e837087749ebc7f53ddea89f2a61e
mcvoramet <vc2543@gmail.com>
2021-11-07 22:18:34 +0700

    deleted unused files

--- PROJECT/gui.py
+++
@@ -1,8 +0,0 @@
-from tkinter import Tk     # from tkinter import Tk for Python 3.x
-from tkinter.filedialog import askopenfilename
-from extract_file import read_file
-import cv2 as cv
-
-Tk().withdraw() # we don't want a full GUI, so keep the root window from appearing
-filename = askopenfilename() # show an "Open" dialog box and return the path to the selected file
-print(filename)
