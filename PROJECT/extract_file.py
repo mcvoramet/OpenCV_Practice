83ea7a6fd35972d12d3a7103442b117909deed21
mcvoramet <vc2543@gmail.com>
2021-11-07 21:11:57 +0700

    add Potato pictures and gui file

--- PROJECT/extract_file.py
+++ PROJECT/extract_file.py
@@ -3,9 +3,9 @@ import matplotlib.pyplot as plt
 import numpy as np
 import os
 
-early_DIR = 'Potato/Train/Potato___Early_blight'   # total 300 images
-late_DIR = 'Potato/Train/Potato___Late_blight'     # total 300 images
-healthy_DIR = 'Potato/Train/Potato___healthy'      # total 300 images
+early_DIR = 'PROJECT/Potato/Train/Potato___Early_blight'   # total 300 images
+late_DIR = 'PROJECT/Potato/Train/Potato___Late_blight'     # total 300 images
+healthy_DIR = 'PROJECT/Potato/Train/Potato___healthy'      # total 300 images
 
 def read_file(path):
     img_file_path = []
@@ -25,6 +25,3 @@ gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 cv.imshow('Gray', gray)
 
 cv.waitKey(0)
-
-
-
